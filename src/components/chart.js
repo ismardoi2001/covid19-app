2acc2d8f58133aaa8b065f6caeedf728addac504
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-25 20:54:00 +0500

    update task

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,6 +4,8 @@ import React from 'react';
     return (
     
             <h1 > Best chart of the Covid19 App </h1>
+            <h2 > Best chart of the Covid19 App </h2>
+            <h3 > Best chart of the Covid19 App </h3>
     )
 }
 export default chart;
