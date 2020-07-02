dec41549261cb8737ea00686267d5bb6c9a2f21c
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-02 20:42:59 +0500

    update task

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export {default as cards} from './cards';
 export{ default as chart} from './chart';
+export{ default as charts} from './charts';
+
 
 export { default as countrypicker} from './countrypicker';
