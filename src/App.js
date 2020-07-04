7523faacf2b27ddd19a3713530c2e7d7221cfd33
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-04 20:59:06 +0500

    update css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import cards from './components/cards';
- import styles from './App.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+}));
 
 function App() {
   
